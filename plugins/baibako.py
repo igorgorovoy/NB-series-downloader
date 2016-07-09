056ccf2966ba23a5932d15d711d25d0e2f0988e2
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2016-07-09 13:05:49 +0300

    add plugins for baibako.tv and coldfilm.ru

---
+++ plugins/baibako.py
@@ -0,0 +1,17 @@
+from core import downloader
+from re import search, split
+
+class baibako(downloader):
+	def __init__(self):
+		super().__init__()
+	def start(self):
+		rss = self.parse('http://baibako.tv/rss2.php', {'feed':'dl', 'passkey':''}).rss('cp1251')
+		if not rss:
+			return 
+		data = []
+		for title, link in rss:
+			series, series_eng, number, quality, *episode = split('\s?\/', title)
+			episode = episode[0].split('(')[0] if len(episode)>0 else ''
+			sn, en = search('s(\d+)e(\d+)', number).groups(1)
+			data.append({'quality':quality, 'series':series, 'episode':episode, 'episode_number':en, 'series_number':sn, 'link':link, 'extension':'torrent'})
+		self.download(data)
---
+++ plugins/coldfilm.py
@@ -0,0 +1,22 @@
+from core import downloader
+from re import search
+
+class coldfilm(downloader):
+	def __init__(self):
+		super().__init__()
+	def start(self):
+		rss = self.parse('http://coldfilm.ru/news/rss').rss()
+		if not rss:
+			return 
+		data = []
+		for t, link in rss:
+			s = search('(.*) (\d+) сезон .*(\d) серия', aa.text)
+			if s:
+				series, sn, en = s.groups(1)
+			else:
+				continue
+			link = self.parse(link).html().select('a[href$=torrent]')[0]
+			q = link.text
+			link = link['href']
+			data.append({'quality':q, 'series':series, 'episode':'', 'episode_number':en, 'series_number':sn, 'link':link, 'extension':'torrent'})
+		self.download(data)
