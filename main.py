5ca6bda5cf55e8981b4bcc90807e7b59adacd4db
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2016-07-18 19:29:41 +0300

    так лучше

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python3
-#encoding: utf-8 
+# encoding: utf-8
 from os import listdir, chdir
 from os.path import dirname, abspath, join
 
@@ -7,6 +7,6 @@ root = dirname(abspath(__file__))
 chdir(join(root, 'plugins'))
 
 for p in [x[:-3] for x in listdir('.') if x.endswith('.py')]:
-	module = __import__('plugins.'+p)
-	_class = getattr(getattr(module, p), p)
+	module = __import__('plugins.' + p)
+	_class = getattr(getattr(module, p), p.title())
 	_class().start()
