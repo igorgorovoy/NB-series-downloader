154e2a0400600c20444505e3433aa34dee785b54
Елизаров Роман Русланович <fantomnotabene@gmail.com>
2016-07-18 19:24:41 +0300

    improvements

--- parser.py
+++ parser.py
@@ -1,66 +1,85 @@
-#encoding: utf-8
-from requests import Session
+# encoding: utf-8
+from requests import Session as rSession
 from bs4 import BeautifulSoup as bs
 from json import loads
 
-class response:
-	def __init__(self, r):
-		self.response = r
-		self.status_code = self.response.status_code
-		if self.status_code==200:
-			self.headers = r.headers
-			self.cookies = r.cookies
-			self.content = r.content
-		
-	def auto(self, encoding='', items=False):
-		if encoding:
-			self.__encode(encoding)
-		else:
-			self.content_type = self.headers['Content-Type']
-			if 'windows-1251' in self.content_type:
-				self.__encode('cp1251')
-			else:
-				self.__encode('utf-8')
-		if 'xml' in self.content_type and 'rss' in self.response.url.lower():
-			return self.rss(encoding, items)
-		elif 'html' in self.content_type or 'xml' in self.content_type:
-			return bs(self.response.text, 'html.parser')
-		elif 'json' in self.content_type:
-			return loads(self.response.text)
-		else:
-			return bs(self.response.text, 'html.parser')
-			
-	def __encode(self, e):
-		self.response.encoding = e
 
-	def html(self, encoding=''):
-		if encoding:
-			self.__encode(encoding)
-		return bs(self.response.text, 'html.parser')
-	
-	def xml(self, encoding=''):
-		return self.html(encoding)
-	
-	def rss(self, encoding='', items=False):
-		if encoding:
-			self.__encode(encoding)
-		return [(x.title.text, x.link.text) for x in bs(self.response.text, 'html.parser').select('item')] if not items else bs(self.response.text, 'html.parser').select('item')
-	
-	def json(self, encoding=''):
-		if encoding:
-			self.__encode(encoding)
-		return loads(self.response.text)
+class Response:
+    # объект ответа сервера
+    # в его основе - обычные атрибуты response-объекта из requests
+    # но с парой интересных методов для парсинга контента страниц
+    def __init__(self, r):
+        self.response = r
+        self.status_code = self.response.status_code
+        if self.status_code == 200:
+            self.headers = r.headers
+            self.cookies = r.cookies
+            self.content = r.content
+            self.content_type = self.headers['Content-Type']
 
-class session:
-	def __init__(self):
-		self.s = Session()
-		
-	def get(self, url, params={}, cookies={}, headers={'User-agent':'Mozilla/5.0'}):
-		return response(self.s.get(url, params=params, cookies=cookies, headers=headers))
+    def auto(self, encoding='', items=False):
+        if encoding:
+            # если указана кодировка - привести к ней
+            self.__encode(encoding)
+        else:
+            # иначе попытаться определить ее самостоятельно
+            if 'windows-1251' in self.content_type:
+                self.__encode('cp1251')
+            else:
+                # в случае неудачи считать что страница в utf-8
+                self.__encode('utf-8')
+        if 'xml' in self.content_type and 'rss' in self.response.url.lower():
+            # если в адресе страницы есть подстрока 'rss', а содержимое в xml
+            # сразу парсим ее как rss
+            # если параметр item == True, то функция rss() не массив пар (заголовок, ссылка), а массив элементов
+            return self.rss(encoding, items)
+        elif 'html' in self.content_type or 'xml' in self.content_type:
+            # по аналогии с rss для html
+            return self.html(encoding)
+        elif 'json' in self.content_type:
+            # и для json
+            return self.json(encoding)
+        else:
+            # если не нашли никаких данных, парсим как html
+            return self.html(encoding)
+
+    def __encode(self, e):
+        # функция приведения кодировки
+        self.response.encoding = e
+
+    def html(self, encoding=''):
+        if encoding:
+            self.__encode(encoding)
+        return bs(self.response.text, 'html.parser')
+
+    def xml(self, encoding=''):
+        return self.html(encoding)
+
+    def rss(self, encoding='', items=False):
+        if encoding:
+            self.__encode(encoding)
+        return [(x.title.text, x.link.text) for x in
+                bs(self.response.text, 'html.parser').select('item')] if not items \
+            else bs(self.response.text, 'html.parser').select('item')
+
+    def json(self, encoding=''):
+        if encoding:
+            self.__encode(encoding)
+        return loads(self.response.text)
 
 
-if __name__=='__main__':
-	link = 'http://habrahabr.ru'
-	s = session()
-	t = s.get(link).html('utf-8').title.text
-	print(t)
+class Session:
+    def __init__(self):
+        # создаем объект request.Session
+        self.s = rSession()
+
+    def get(self, url, params={}, cookies={}, headers={'User-agent': 'Mozilla/5.0'}):
+        return Response(self.s.get(url, params=params, cookies=cookies, headers=headers))
+
+
+if __name__ == '__main__':
+    res = Session().get('http://habrahabr.ru').auto().select('a.post-preview__link')
+    for post_preview in res:
+        title = post_preview.text
+        link = post_preview['href']
+        print('%s => %s' % (title, link))
